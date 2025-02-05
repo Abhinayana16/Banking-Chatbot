55593a5a269b98f3263b5d805b31b03a029daf75
Abhinayana Vanaparthi <abhinayanavanaparthi@gmail.com>
2025-02-05 10:03:09 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbotmodel.h5')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word)  for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words= clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda  x:x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    result = ""
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+st.title("Basic Banking Chatbot System")
+st.write("Feel free to ask any queries about banking processes.")
+
+user_input = st.text_input("You:")
+if st.button("Send"):
+    if user_input.lower() == "bye" or user_input.lower() == "goodbye":
+        response_intents = predict_class(user_input)
+        bot_response = get_response(response_intents, intents)
+        st.write("Bot:", bot_response)
+        st.write("The program ends here!")
+    else:
+        response_intents = predict_class(user_input)
+        bot_response = get_response(response_intents, intents)
+        st.write("Bot:", bot_response)
