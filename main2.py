50b3ee5cdd181fb2b807407e10b32061149319c1
Abhinayana Vanaparthi <abhinayanavanaparthi@gmail.com>
2025-04-05 16:24:02 +0530

    main2.py

--- main2.py
+++ main2.py
@@ -1,17 +1,24 @@
 import streamlit as st
 import random
-import json
 import pickle
 import numpy as np
-
 import nltk
 from nltk.stem import WordNetLemmatizer
-
 from tensorflow.keras.models import load_model
+from pymongo import MongoClient
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 lemmatizer = WordNetLemmatizer()
 
-intents = json.loads(open('intents.json').read())
+# Connect to MongoDB and load intents
+client = MongoClient("mongodb://localhost:27017/")
+db = client["chatbotdb"]
+collection = db["intents"]
+intents_cursor = collection.find()
+intents = {"intents": list(intents_cursor)}
 
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
@@ -19,11 +26,11 @@ model = load_model('chatbotmodel.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word)  for word in sentence_words]
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
 
 def bag_of_words(sentence):
-    sentence_words= clean_up_sentence(sentence)
+    sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
     for w in sentence_words:
         for i, word in enumerate(words):
@@ -35,9 +42,8 @@ def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-
-    results.sort(key=lambda  x:x[1], reverse=True)
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
@@ -53,17 +59,15 @@ def get_response(intents_list, intents_json):
             break
     return result
 
-st.title("Basic Banking Chatbot System")
-st.write("Feel free to ask any queries about banking processes.")
+st.title("\U0001F916 Banking Chatbot")
+st.markdown("Ask me anything about banking!")
 
-user_input = st.text_input("You:")
-if st.button("Send"):
-    if user_input.lower() == "bye" or user_input.lower() == "goodbye":
-        response_intents = predict_class(user_input)
-        bot_response = get_response(response_intents, intents)
-        st.write("Bot:", bot_response)
-        st.write("The program ends here!")
+user_input = st.text_input("You:", "")
+
+if user_input:
+    ints = predict_class(user_input)
+    if ints:
+        response = get_response(ints, intents)
+        st.text_area("Bot:", response, height=100)
     else:
-        response_intents = predict_class(user_input)
-        bot_response = get_response(response_intents, intents)
-        st.write("Bot:", bot_response)
+        st.text_area("Bot:", "Sorry, I didn't get that. Could you please rephrase?", height=100)
